0a07260a757e18873b43af5e4deb53dca4ba9889
zhangxujie2018 <jasonzhang666@163.com>
2026-07-26 16:12:18 +0800

    test: configure mobile platform fakes

--- mobile_app/test/widget_test.dart
+++ mobile_app/test/widget_test.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_mic_bridge/main.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 
 void main() {
   testWidgets('shows connection form', (tester) async {
+    SharedPreferencesAsyncPlatform.instance =
+        InMemorySharedPreferencesAsync.empty();
     await tester.pumpWidget(const MobileMicApp());
     expect(find.text('Mobile Mic Bridge'), findsOneWidget);
     expect(find.text('开始传输'), findsOneWidget);
